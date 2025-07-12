0be4febcf4b0a2dc96986092a9556024a1c1e474
GhazyGyfar <ghazygyfar@gmail.com>
2025-07-12 08:52:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
+
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+
 st.header("This is a header with a divider", divider="gray")
